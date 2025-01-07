2fd0e67ed81a658c99e065e617816e55758da7a7
Joe Helle <25853389+dievus@users.noreply.github.com>
2025-01-06 21:16:47 -0500

    Update s3.py

--- functions/s3.py
+++ functions/s3.py
@@ -11,23 +11,33 @@ def bucket_check(s3_client, args):
         s3_client.get_bucket_policy(Bucket=args.bucket)
     except s3_client.exceptions.NoSuchBucket:
         print(
-            'The bucket provided does not exist, or is not accessible by this user account.')
+            "The bucket provided does not exist, or is not accessible by this user account."
+        )
         quit()
     except ClientError as e:
         if e.response["Error"]["Code"] == "AccessDenied":
-            print(f'IAM user account is missing the s3:GetBucketPolicy permission. Review the example policy in the repo and modify appropriately. Quitting...')
+            print(
+                f"IAM user account is missing the s3:GetBucketPolicy permission. Review the example policy in the repo and modify appropriately. Quitting..."
+            )
             quit()
 
 
 def revert_bucket_policy(s3_client, bucket_resource, args):
-    print(
-        f'\nReverting s3 bucket policy for {args.bucket} back to default deny all...')
-    revert_policy_document = {"Version": "2012-10-17", "Statement": [{"Effect": "Deny", "Principal": {
-        "AWS": "*"}, "Action": ["s3:DeleteObject", "s3:PutObject"], "Resource": bucket_resource}]}
+    print(f"\nReverting s3 bucket policy for {args.bucket} back to default deny all...")
+    revert_policy_document = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Deny",
+                "Principal": {"AWS": "*"},
+                "Action": ["s3:DeleteObject", "s3:PutObject"],
+                "Resource": bucket_resource,
+            }
+        ],
+    }
     policy_document_str = json.dumps(revert_policy_document)
     try:
-        s3_client.put_bucket_policy(
-            Policy=policy_document_str, Bucket=args.bucket)
+        s3_client.put_bucket_policy(Policy=policy_document_str, Bucket=args.bucket)
         print("Policy reverted successfully.")
     except Exception:
         print("Failed to revert policy. Update it manually in AWS console.")
@@ -39,12 +49,23 @@ def bucket_enum(args):
         found_users = 0
         timeout = int(args.timeout) if args.timeout else 0
         print(
-            f'Finding valid IAM usernames in {args.account} utilizing the S3 method.\nPlease be patient...\n')
+            f"Finding valid IAM usernames in {args.account} utilizing the S3 method.\nPlease be patient...\n"
+        )
         session = boto3.Session(
-            aws_access_key_id=args.accesskey,
-            aws_secret_access_key=args.secretkey
+            aws_access_key_id=args.accesskey, aws_secret_access_key=args.secretkey
         )
         s3_client = session.client("s3")
+        sts_client = session.client("sts")
+        try:
+            sts_client.get_caller_identity()
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "SignatureDoesNotMatch":
+                print(
+                    "\nInvalid Signature. Access key and secret key may be incorrect.\nQuitting..."
+                )
+                quit()
+            else:
+                print(f"\nSome issue occurred: {e}.\nQuitting...")
         bucket_check(s3_client, args)
 
         def process_username(username):
@@ -52,7 +73,16 @@ def bucket_enum(args):
             nonlocal found_users
             arn_val = f"arn:aws:iam::{args.account}:user/{username}"
             policy_document = {
-                "Version": "2012-10-17", "Statement": [{"Effect": "Deny", "Principal": {"AWS": arn_val}, "Action": ["s3:DeleteObject", "s3:PutObject"], "Resource": bucket_resource}]}
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Deny",
+                        "Principal": {"AWS": arn_val},
+                        "Action": ["s3:DeleteObject", "s3:PutObject"],
+                        "Resource": bucket_resource,
+                    }
+                ],
+            }
             policy_document_str = json.dumps(policy_document)
             try:
                 if timeout:
@@ -68,11 +98,13 @@ def bucket_enum(args):
             except ClientError as e:
                 if e.response["Error"]["Code"] == "SignatureDoesNotMatch":
                     print(
-                        '\nInvalid Signature. Access key and secret key may be incorrect.\nQuitting...')
+                        "\nInvalid Signature. Access key and secret key may be incorrect.\nQuitting..."
+                    )
                     raise KeyboardInterrupt
                 if e.response["Error"]["Code"] == "AccessDenied":
                     print(
-                        f'\nAccess denied for updating policy in {args.rolename}. Quitting...')
+                        f"\nAccess denied for updating policy in {args.rolename}. Quitting..."
+                    )
                     raise KeyboardInterrupt
                 if e.response["Error"]["Code"] == "MalformedPolicyDocument":
                     pass  # This means the IAM user is not real.
@@ -81,25 +113,27 @@ def bucket_enum(args):
             except EndpointConnectionError:
                 print("\nNetwork connection issue. Quitting...")
                 raise KeyboardInterrupt
+
         with open(args.read) as possible_users:
             usernames = [line.strip() for line in possible_users]
-
         # Adjust max_workers as needed
         with ThreadPoolExecutor(max_workers=10) as executor:
-            futures = {executor.submit(
-                process_username, username): username for username in usernames}
+            futures = {
+                executor.submit(process_username, username): username
+                for username in usernames
+            }
             try:
                 for future in as_completed(futures):
                     future.result()  # Will raise exceptions from threads if any
             except KeyboardInterrupt:
                 print(
-                    "\nKeyboard interrupt detected. Stopping execution. This may take a minute.")
+                    "\nKeyboard interrupt detected. Stopping execution. This may take a minute."
+                )
                 # Cancel remaining threads
                 executor.shutdown(wait=False)
                 raise
         revert_bucket_policy(s3_client, bucket_resource, args)
         print(f"\n{found_users} valid IAM usernames found. Quitting...")
-
     except KeyboardInterrupt:
-        print('\nOperation interrupted.')
+        print("\nOperation interrupted.")
         revert_bucket_policy(s3_client, bucket_resource, args)
