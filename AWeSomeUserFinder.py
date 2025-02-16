780f9667d1b52d65376957b1198b5e7e3e913056
Joe Helle <25853389+dievus@users.noreply.github.com>
2025-02-16 12:48:10 -0500

    Update AWeSomeUserFinder.py

--- AWeSomeUserFinder.py
+++ AWeSomeUserFinder.py
@@ -4,6 +4,7 @@ import sys
 from functions.assumerole import user_enum
 from functions.banner import *
 from functions.s3 import bucket_enum
+from functions.sns import sns_handler
 from functions.spray import spray
 
 
@@ -19,22 +20,31 @@ def options():
     requiredArgs.add_argument(
         "-a", "--account", help="AWS account to check for IAM users")
     requiredArgs.add_argument(
-        "-r", "--read", help="Reads usernames from a file to test")
+        "-rf", "--read", help="Reads usernames from a file to test")
+    requiredArgs.add_argument("-ak", "--accesskey",
+                              help="Access key for enumerating users")
+    requiredArgs.add_argument("-sk", "--secretkey",
+                              help="Secret key for enumerating users")
     opt_parser.add_argument(
         "-i", "--iam", help="Uses IAM policy modification for enumeration", action="store_true")
     opt_parser.add_argument(
         "-s3", "--s3enum", help="Uses s3 bucket policy modification for enumeration", action="store_true")
     opt_parser.add_argument(
         "-b", "--bucket", help="Bucket name to use for s3 policy")
-    opt_parser.add_argument("-ak", "--accesskey",
-                            help="Access key for enumerating users")
-    opt_parser.add_argument("-sk", "--secretkey",
-                            help="Secret key for enumerating users")
+    opt_parser.add_argument(
+        "-sns", "--snsenum", help="Uses SNS policy modification for enumeration", action="store_true"
+    )
+    opt_parser.add_argument(
+        "-ta", "--topicarn", help="Topic Arn to modify"
+    )
+    opt_parser.add_argument(
+        "-r", "--region", help="Specify a region to use with SNS enumeration"
+    )
     opt_parser.add_argument(
         "-s", "--spray", help="Password spray a list of account names", action="store_true")
     opt_parser.add_argument("-p", "--password", help="Password to spray")
-    opt_parser.add_argument(
-        "-f", "--find", help="Find valid AWS IAM account names", action="store_true")
+    # opt_parser.add_argument(
+    #     "-f", "--find", help="Find valid AWS IAM account names", action="store_true")
     opt_parser.add_argument(
         "-rn", "--rolename", help="Role name to add to the assume policy document")
     opt_parser.add_argument(
@@ -53,10 +63,12 @@ if __name__ == "__main__":
     try:
         banner()
         options()
-        if args.find:
+        if args.iam:
             user_enum(args)
         elif args.s3enum:
             bucket_enum(args)
+        elif args.snsenum:
+            sns_handler(args)
         elif args.spray:
             spray(args)
     except KeyboardInterrupt:
