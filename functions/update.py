ea1f6c2bc08ead45106b2ff6c408a83fb20db26c
Joe Helle <25853389+dievus@users.noreply.github.com>
2025-04-22 21:51:38 -0400

    Update update.py

--- functions/update.py
+++ functions/update.py
@@ -1,12 +1,13 @@
 import subprocess
 import os
 def update_repo():
+    print('[+] Attempting to update repository with latest version from Github.')
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     repo_path = os.path.abspath(os.path.join(cur_dir, '..'))
     try:
         subprocess.run(['git', 'rev-parse', '--is-injside-work-tree'], cwd=repo_path, check=True, stdout=subprocess.DEVNULL)
         subprocess.run(['git', 'checkout', 'main'], cwd=repo_path, check=True)
         subprocess.run(['git', 'pull'], cwd=repo_path, check=True)
-        print(f"Repository in {repo_path} updated to the latest version.")
+        print(f"[✓] Repository in {repo_path} updated to the latest version.")
     except subprocess.CalledProcessError as e:
-        print(f"Error updating the repository in {repo_path}: {e}")        
+        print(f"[-] Error updating the repository in {repo_path}: {e}")        
