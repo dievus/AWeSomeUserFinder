adb983633e4cf079e210c991e942b827718d38e1
Joe Helle <25853389+dievus@users.noreply.github.com>
2025-04-22 21:55:16 -0400

    Update update.py

--- functions/update.py
+++ functions/update.py
@@ -8,6 +8,6 @@ def update_repo():
         subprocess.run(['git', 'rev-parse', '--is-injside-work-tree'], cwd=repo_path, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run(['git', 'checkout', 'main'], cwd=repo_path, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run(['git', 'pull'], cwd=repo_path, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print(f"[✓] Repository in {repo_path} updated to the latest version.")
+        print(f"\n[✓] Repository in {repo_path} updated to the latest version.")
     except subprocess.CalledProcessError as e:
-        print(f"[-] Error updating the repository in {repo_path}: {e}")        
+        print(f"\n[-] Error updating the repository in {repo_path}: {e}")        
